350782e290150ea08e6f0f052bcf6e2be221fb52
WagmiVentures <94269487+WagmiVentures@users.noreply.github.com>
2022-03-14 13:09:54 +1000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import "./styles.css";
+
+const animals = [
+  {
+    name: "Lion",
+    number: 3,
+    eats: ["zebra", "antelope", "buffalo", "hippopotamus"]
+  },
+  {
+    name: "Tiger",
+    number: 5,
+    eats: ["moose", "deer", "buffalo"]
+  },
+  {
+    name: "Giraffe",
+    number: 6,
+    eats: ["leaves", "twigs"]
+  },
+  {
+    name: "Elephant",
+    number: 4,
+    eats: ["grass", "leaves", "flowers", "fruit"]
+  },
+  {
+    name: "Monkey",
+    number: 10,
+    eats: ["fruit", "leaves", "vegetables", "insects"]
+  },
+  {
+    name: "Lemur",
+    number: 15,
+    eats: ["fruit", "leaves", "insects"]
+  },
+  {
+    name: "Rhinoceros",
+    number: 2,
+    eats: ["grass", "shoots", "leaves", "berries"]
+  }
+];
+
+function AnimalComponet(props) {
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <p>{props.number}</p>
+      <p>{props.eats.join(", ")}</p>
+    </div>
+  );
+}
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+
+      {animals.map((animal) => (
+        <AnimalComponet {...animal} />
+      ))}
+    </div>
+  );
+}
